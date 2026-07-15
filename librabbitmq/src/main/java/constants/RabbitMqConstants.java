726447b71d3fb99978cae5c7c8be275ffc1a0cca
Thamyrescordeiro <101646936+Thamyrescordeiro@users.noreply.github.com>
2026-07-15 09:15:43 -0300

    Add files via upload

---
+++ librabbitmq/src/main/java/constants/RabbitMqConstants.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class RabbitMqConstants {
+    public static final String STOCK_QUEUE = "STOCK";
+    public static final String STOCK_PRICE = "PRICE";
+}
---
+++ librabbitmq/src/main/java/dto/StockDto.java
@@ -0,0 +1,9 @@
+package dto;
+
+import java.io.Serializable;
+
+public class StockDto implements Serializable {
+
+    public String productcode;
+    public int quantity;
+}
